6467b4f60a7d31dd097ecaf6a5f9958c420211fe
lucas.sessi <lsessi@digiboard.com.br>
2024-11-22 14:52:35 -0400

    mais um ajuste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'https://my-notes-backend-pearl.vercel.app'
-  apiUrl: 'http://192.168.0.58:4000'
+  apiUrl: 'http://localhost:4000'
 };
